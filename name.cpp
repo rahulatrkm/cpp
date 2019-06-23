eaf6c1947a92e7f7d342f3a284083226c1f89f59
Rahul <rahulatrkm@gmail.com>
2019-06-24 01:29:37 +0530

    completed upto topic 22

---
+++ name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hey, writing std:: is pain, ";
+  cout << "change the program so I don't have to write it.\n";
+
+  return 0;
+}
